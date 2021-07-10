bcfbfdcdc0515f49827da4da69f9a07e663332d1
kirimaru <nainaistar@gmail.com>
2021-07-10 11:53:40 +0000

    [Add] headlessモードの確認

--- sample.py
+++ sample.py
@@ -4,7 +4,8 @@ from playwright.async_api import async_playwright
 async def main():
     async with async_playwright() as p:
         for browser_type in [p.chromium, p.firefox, p.webkit]:
-            browser = await browser_type.launch()
+            # Dockerでは画面描画する機能はないので、Docker上で開発するのは現実的ではない
+            browser = await browser_type.launch(headless=True)
             page = await browser.new_page()
             await page.goto('http://whatsmyuseragent.org/')
             await page.screenshot(path=f'example-{browser_type.name}.png')
