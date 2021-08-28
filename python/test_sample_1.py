14f41cc1b4aef69455016270ff9f08acf5927962
kirimaru <nainaistar@gmail.com>
2021-08-28 13:38:11 +0000

    config.iniと読み込み

--- python/test_sample_1.py
+++ python/test_sample_1.py
@@ -1,5 +1,20 @@
 from playwright.sync_api import Page
 
+import configparser
+
+# --------------------------------------------------
+# configparserの宣言とiniファイルの読み込み
+# --------------------------------------------------
+config_ini = configparser.ConfigParser()
+config_ini.read('config.ini', encoding='utf-8')
+
+read_default = config_ini['DEFAULT']
+username = read_default.get('username')
+password = read_default.get('password')
+
+print(f"username='{username}', password='{password}'")
+
+
 def test_rakuten(page: Page):
     page.goto("https://www.rakuten-sec.co.jp/")
     index = 0
