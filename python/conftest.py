ae2ff5119860373930795897f8f3c90aefa0404d
kirimaru <nainaistar@gmail.com>
2021-08-29 01:49:09 +0000

    [Add] クラスの挙動確認

--- python/conftest.py
+++ python/conftest.py
@@ -3,4 +3,5 @@ import pytest
 @pytest.fixture(scope="session")
 def props():
     from TestProperties import TestProperties
+    # return TestProperties(file_path='test_config.ini', section='TEST')
     return TestProperties()
