de707958ce651ce9e341706704995afc54df6d31
kirimaru <nainaistar@gmail.com>
2021-09-03 12:34:11 +0000

    test

--- python/test_sample_1.py
+++ python/test_sample_1.py
@@ -1,7 +1,13 @@
 from playwright.sync_api import Page
 
 def test_rakuten(props, page: Page):
-    print(f"username='{props.username}', password='{props.password}'")
-    page.goto("https://www.rakuten-sec.co.jp/")
-    index = 0
-    page.screenshot(path=f'example-{index}.png')
+    # print(f"username='{props.username}', password='{props.password}'")
+    # page.goto("https://www.rakuten-sec.co.jp/")
+    # index = 0
+    # page.screenshot(path=f'example-{index}.png')
+    
+
+    page.goto("http://7dc9-240f-6d-3fea-1-d095-fe1c-71d9-3d7b.ngrok.io/tutorial")
+    page.screenshot(path=f'test.png', full_page=True)
+
+    # pytest --browser chromium --browser webkit --browser firefox
