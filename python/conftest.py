451e183625ffc935989563a93a56b167331da826
kirimaru <nainaistar@gmail.com>
2021-09-18 13:53:52 +0000

    false

--- python/conftest.py
+++ python/conftest.py
@@ -3,8 +3,15 @@ import os
 
 def pytest_addoption(parser):
     """add commandline options"""
-    parser.addoption('--env', action='store', default='IT',
-                     help='pc or sp')
+    parser.addoption('--env', 
+                    action='store', 
+                    default='IT',
+                    help='pc or sp')
+    parser.addoption('--local', 
+                    action='store_true', 
+                    default=False,
+                    help='local')
+
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -12,6 +19,8 @@ def props(request):
     from TestProperties import TestProperties
 
     os.environ['ENV'] = request.config.getoption("--env")
+    print("*****")
+    print(request.config.getoption("--local"))
 
     # return TestProperties(file_path='test_config.ini', section='TEST')
     return TestProperties(section=request.config.getoption("--env"))
